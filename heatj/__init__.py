f590c76854d02acd877ad9fd305cafb65da7cc22
Alex Kerr <ajkerr0@gmail.com>
2018-04-18 00:01:41 -0500

    Add basic plt.mode_evolution which works but runs into trouble when the zero real part goes to zero

--- heatj/__init__.py
+++ heatj/__init__.py
@@ -2,3 +2,4 @@
 
 from .lattice import *
 from .simple1d import *
+from . import plot
--- heatj/plot.py
+++ heatj/plot.py
@@ -20,3 +20,69 @@ def single_modes(modes):
             plt.scatter(x+push, y, c='C0', s=50)
             
     plt.show()
+    
+def mode_evolution(lattice, mindex, sindices, g):
+    """Plot the evolution of an eigenmode (real and imaginary parts separately)
+    as a function of the dissipation from the heat baths.
+    
+    Arguments
+    ---------
+    lattice  : Lattice
+        Lattice object with modes of interest
+    mindex   : int
+        Index of the mode in the unperturbed (zero-damping) problem
+    sindices : array-like
+        Array of indices of the coordinates of interest
+    g        : array-like
+        Evolution of the gammas
+        
+    There could be problems if the first gamma is not zero.
+
+    """
+    
+    s = np.array(sindices)
+    g = np.array(g)
+    
+    
+    # get the undamped eigenmode 
+    uval, uvec = np.linalg.eig(lattice.k)
+    umode = uvec[:,mindex]
+    
+    # evolve the eigenmode
+    pos = np.zeros((g.shape[0], s.shape[0]))
+    
+    for i,gamma in enumerate(g):
+        
+        # determine the eigenmode(s)
+        lattice.gamma = gamma
+        val, vec = lattice._calculate_evec()
+        
+        # find the eigenmode that corresponds to the previous one
+        a = vec[:lattice.n].imag.T
+        print(a)
+        print(np.linalg.norm(a, axis=1))
+        a = a/np.linalg.norm(a, axis=1)[:,None]
+#        print(a/np.linalg.norm(a, axis=1)[:,None])
+#        print(umode)
+#        print(a/np.linalg.norm(a, axis=1)[:,None]-umode)
+#        print(np.linalg.norm(a/np.linalg.norm(a, axis=1)[:,None]-umode, axis=1))
+        mindex = np.argmin(np.linalg.norm(a-umode, 
+                                          axis=1))
+        pos[i] = a[mindex][s]
+        
+    # plot
+    x = np.arange(s.shape[0])
+    
+    for site in x:
+        plt.plot(pos[:,site]+site, g, '-x')
+        plt.axvline(x=site, ymin=np.min(g), ymax=np.max(g), linestyle='dashed')
+        
+    plt.show()
+        
+        
+    
+def norm(vec):
+    return vec/np.linalg.norm(vec)
+    
+    
+    
