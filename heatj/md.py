73d6ef0ae155f13f544dd79680f047f14cd79b89
Alex Kerr <ajkerr0@gmail.com>
2019-08-13 17:11:20 -0500

    Add Green's function solution to the positions/velocities of heatj.Lattice positions

--- heatj/md.py
+++ heatj/md.py
@@ -3,6 +3,12 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 
+class MDBatch(object):
+    
+    def __init__(self, t, y):
+        self.t = t
+        self.y = y
+
 def perform_md(lattice, t_span, nt, temp1, temp2, solver_options={}):
     """
     Return the scipy IVP solver batch object for the lattice dynamics
@@ -65,9 +71,44 @@ def perform_md(lattice, t_span, nt, temp1, temp2, solver_options={}):
         
         return ydot
     
-    sol = solve_ivp(func, t_span, y0, jac=None, t_eval=times, **solver_options)
+    return solve_ivp(func, t_span, y0, jac=None, t_eval=times, **solver_options)
+
+def perform_md_gf(lattice, t_span, nt, temp1, temp2):
+    """
+    Return the Green's function solution to the positions/velocities
+    of lattice objects.
+    """
+        
+    n = lattice.mass.shape[0]
+    nd = lattice.drivers.shape[1]  # number of drivers on each side
+    dim = lattice.dim
     
-    return sol
+    times = np.linspace(*t_span, num=nt)
+    
+    force = np.zeros((nt, n*dim, 1))
+    for i in np.arange(dim):
+        force[:,dim*lattice.drivers[0] + i] = np.sqrt(2.*lattice.gamma*temp1)*np.random.randn(nt, nd, 1)
+        force[:,dim*lattice.drivers[1] + i] = np.sqrt(2.*lattice.gamma*temp2)*np.random.randn(nt, nd, 1)
         
+    y = np.zeros((2*n, nt))
         
+    for t_i in np.arange(1, nt):
+        
+        t = times[:t_i]
+    
+        gf = lattice.val[:,None]*(-(t[-1] - t)[:,None,None]*np.ones((t_i, 2*n, n)))
+        gf = lattice.coeffs[None,:]*np.exp(gf)
+        q = np.dot(lattice.vec[:n,:], gf).reshape(t_i, n, n)
+        qdot = np.dot(lattice.vec[n:,:], gf).reshape(t_i, n, n)
+        
+        f = force[:t_i,:,:]
+        
+        q = np.matmul(q, f)[:,:,0].T.real
+        qdot = np.matmul(qdot, f)[:,:,0].T.real
+        
+
+        y[:n,t_i] = np.sum(q, axis=1)
+        y[n:,t_i] = np.sum(qdot, axis=1)
+    
+    return MDBatch(times, y)
         
